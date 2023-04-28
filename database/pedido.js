421d4dac4710165a5dd46f0ea3a0d43ff6be19c9
souzaschuery <souzaschuery@gmail.com>
2023-04-28 10:18:52 -0300

    criação do pedido

---
+++ database/pedido.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("./database");
+const Cliente = require("./cliente");
+
+const Pedido = connection.define("pedido", {
+    codigo: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    quantidade: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+})
+
+Cliente.hasMany(Pedido, { onDelete: "CASCADE" });
+Pedido.belongsTo(Cliente);
+
+
+module.exports = Pedido;
