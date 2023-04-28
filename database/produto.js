9f0f99d888e8850f2399f946ca53f99379631f2f
UtescherIntrieri <18ianaui1999@gmail.com>
2023-04-28 11:14:40 -0300

    produto corrections

--- database/produto.js
+++ database/produto.js
@@ -1,33 +1,31 @@
 const { DataTypes } = require("sequelize");
 const { connection } = require("./database");
 
-const Produto = connection.define("produto",
-    new Schema({
-        nome: {
-           type: DataTypes.STRING,
-           allowNull: false
-        },
-        preco: {
-            type: Number,
-           allowNull: false
-        },
-        descricao: {
-            type: DataTypes.STRING(150),
-            allowNull: false
-        },
-        desconto: {
-           type: Number,
-           allowNull: false
-        },
-        dataDesconto: {
-           type: Date,
-           allowNull: false
-        },
-        caregoria:{
-           type: DataTypes.STRING,
-           allowNull: true
-        },
-    })
-)
+const Produto = connection.define("produto", {
+   nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+   },
+   preco: {
+       type: DataTypes.INTEGER,
+      allowNull: false
+   },
+   descricao: {
+       type: DataTypes.STRING(150),
+       allowNull: false
+   },
+   desconto: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+   },
+   dataDesconto: {
+      type: DataTypes.DATE,
+      allowNull: false
+   },
+   caregoria:{
+      type: DataTypes.STRING,
+      allowNull: true
+   },
+ })
     
 module.exports = Produto
---
+++ routes/produtos.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const Produto = require("../database/produto");
+
+const router = Router();
+
+
+
+module.exports = router;
